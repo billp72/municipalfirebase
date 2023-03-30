4e424011cace75df40146f66b15bea97dd07242d
billpope <epop.bill@gmail.com>
2023-03-30 18:22:47 -0400

    added end points

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,44 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
+admin.initializeApp();
+const auth = admin.auth();
+const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onCall((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  //response.send("Hello from Firebase!");
-  return ["hello", "goodbye"];
+const administer = {
+  admin: true,
+  municipality: ""
+}
+
+const resident = {
+  admin: false,
+  municipality: ""
+}
+export const adminLevel = functions.https.onCall(async(request, response) => {
+  //functions.logger.info("Hello logs!", {structuredData: true});
+  const claims = administer.municipality = request.municipality;
+  await auth.setCustomUserClaims(request.uid,  claims);
+  const user:any = await auth.getUser(request.uid);
+  db.collection('users').doc('/'+user.customClaims['municipality']+'/').update({[request.uid]:{
+    ...request
+  }})
+  return user;
 });
+
+export const residentLevel = functions.https.onCall(async(request, response) => {
+  //functions.logger.info("Hello logs!", {structuredData: true});
+  const claims = resident.municipality = request.municipality;
+  await auth.setCustomUserClaims(request.uid,  claims);
+  const user:any = await auth.getUser(request.uid)
+  db.collection('users').doc('/'+user.customClaims['municipality']+'/').update({[request.uid]:{
+    ...request
+  }})
+  return user;
+});
+
+export const getCustomClaim = functions.https.onCall(async(request, response) => {
+  const operatorUser = await admin.auth().getUser(request.uid);
+  return operatorUser.customClaims;
+})
