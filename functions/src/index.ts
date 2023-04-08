5dd8bbd40cacf0c026e996eae1a12124845ae611
billpope <epop.bill@gmail.com>
2023-04-08 14:48:37 -0400

    fetch all use alarms

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,44 +9,73 @@ const db = admin.firestore();
 //
 const administer = {
   admin: true,
-  municipality: ""
-}
+  municipality: "",
+};
 
 const resident = {
   admin: false,
-  municipality: ""
-}
-export const adminLevel = functions.https.onCall(async(request, response) => {
+  municipality: "",
+};
+export const adminLevel = functions.https.onCall(async (request, response) => {
   administer.municipality = request.municipality;
-  
-  await auth.setCustomUserClaims(request.uid,  administer);
-  const user:any = await auth.getUser(request.uid)
-  const municipal = user['customClaims']['municipality'];
-  const docref = db.collection('users').doc(municipal);
+
+  await auth.setCustomUserClaims(request.uid, administer);
+  const user: any = await auth.getUser(request.uid);
+  const municipal = user["customClaims"]["municipality"];
+  const docref = db.collection("users").doc(municipal);
   request.admin = true;
-  docref.set({[request.uid]:{
-    ...request
-  }}, {merge:true})
+  docref.set(
+    {
+      [request.uid]: {
+        ...request,
+      },
+    },
+    { merge: true }
+  );
 
   return user;
 });
 
-export const residentLevel = functions.https.onCall(async(request, response) => {
-  resident.municipality = request.municipality;
+export const residentLevel = functions.https.onCall(
+  async (request, response) => {
+    resident.municipality = request.municipality;
 
-  await auth.setCustomUserClaims(request.uid,  resident);
-  const user:any = await auth.getUser(request.uid)
-  const municipal = user['customClaims']['municipality'];
-  const docref = db.collection('users').doc(municipal);
-  request.admin = false;
-  docref.set({[request.uid]:{
-    ...request
-  }}, {merge:true})
+    await auth.setCustomUserClaims(request.uid, resident);
+    const user: any = await auth.getUser(request.uid);
+    const municipal = user["customClaims"]["municipality"];
+    const docref = db.collection("users").doc(municipal);
+    request.admin = false;
+    docref.set(
+      {
+        [request.uid]: {
+          ...request,
+        },
+      },
+      { merge: true }
+    );
 
-  return user;
-});
+    return user;
+  }
+);
 
-export const getCustomClaim = functions.https.onCall(async(data, context) => {
+export const getCustomClaim = functions.https.onCall(async (data, context) => {
   const operatorUser = await admin.auth().getUser(data.uid);
   return operatorUser?.customClaims || "";
-})
+});
+
+export const getUserAlerts = functions.https.onCall(async (data, context) => {
+  const docRef = db.collection("topics");
+  const all = [];
+  let snapshot = await docRef.get();
+  let arr = snapshot.docs.map((doc) => {
+    return doc.data();
+  });
+  for (let itm in arr) {
+    for (let b in arr[itm]) {
+      if (b === data.uid) {
+        all.push(arr[itm][b]);
+      }
+    }
+  }
+  return all;
+});
